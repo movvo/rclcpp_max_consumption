dd024d5799b6551186237b26584c9d744839c2f7
Albert Arlà <albert.arla@movvo.eu>
2023-10-10 11:35:08 +0200

    update stella branch + bugs

--- component_stella_docker/docker/run_components.sh
+++ component_stella_docker/docker/run_components.sh
@@ -5,25 +5,39 @@ if [ $# -eq 0 ]
     echo "usage is ./run_components <num_processes>"
     exit 1
 fi
-
+echo $(dirname $(dirname $(realpath $0)))
 docker run -it --rm -d\
     --env=ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
     --net=host \
-    --name=component_stella_docker \
-    component_stella_docker:latest \
+    --volume=$(dirname $(dirname $(realpath $0)))/inputs:/inputs:rw \
+    --name=stella_vslam-ros-socket \
+    stella_vslam-ros-socket:latest \
     ros2 run rclcpp_components component_container_mt
+
+docker exec -it -d stella_vslam-ros-socket /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 odom base_link"
+docker exec -it -d stella_vslam-ros-socket /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link cam0"
+
 
 for (( c=1; c<=$1; c++ ))
 do
   echo "Load process $i"
-  docker exec -it -d component_stella_docker /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 component load /ComponentManager component component_stella::Component"
-  sleep 0.2
+  docker exec -it -d stella_vslam-ros-socket /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 component load /ComponentManager stella_vslam_ros stella_vslam_ros::System -p vocab_file_path:="/inputs/orb_vocab.fbow" -p setting_file_path:="/inputs/euroc_example.yaml" -p map_db_path_out:="/inputs/map.db" -r /camera/image_raw:=/cam0/image_raw" #-p viewer:="socket_publisher"
+  sleep 0.8
 done
 
 sleep 1
 echo "Starting processes"
-docker exec -it -d component_stella_docker /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 service call /component/change_state std_srvs/srv/Trigger"
+docker run -it --rm -d\
+    --env=ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
+    --net=host \
+    --volume=$(dirname $(dirname $(realpath $0)))/inputs:/inputs:rw \
+    --name=stella_vslam-rosbag \
+    stella_vslam-rosbag:latest \
+    /inputs/start_stella_vslam-rosbag.sh
+# docker exec -it -d component_stella_docker /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 service call /component/change_state std_srvs/srv/Trigger"
 
 read -p "Press [Enter] key to finish all processes and kill the docker"
 
-docker kill component_stella_docker
+docker kill stella_vslam-ros-socket
+docker kill stella_vslam-rosbag
+
--- component_stella_docker/docker/run_processes.sh
+++ component_stella_docker/docker/run_processes.sh
@@ -6,25 +6,36 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-
-docker run -it --rm  -d\
+docker run -it --rm -d\
     --env=ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
     --net=host \
-    --name=component_docker \
-    component_docker:latest \
+    --volume=$(dirname $(dirname $(realpath $0)))/inputs:/inputs:rw \
+    --name=stella_vslam-ros-socket \
+    stella_vslam-ros-socket:latest \
     tail -f /dev/null
+
+docker exec -it -d stella_vslam-ros-socket /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 odom base_link"
+docker exec -it -d stella_vslam-ros-socket /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link cam0"
 
 for (( c=1; c<=$1; c++ ))
 do
   echo "Load process $i"
-  docker exec -it -d component_docker /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 run component Component"
+  docker exec -it -d stella_vslam-ros-socket /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 run stella_vslam_ros run_slam -v /inputs/orb_vocab.fbow -c /inputs/euroc_example.yaml --map-db-out /inputs/map.db --ros-args -r /camera/image_raw:=/cam0/image_raw"
   sleep 0.2
 done
 
 sleep 1
 echo "Starting processes"
-docker exec -it -d component_docker /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 service call /component/change_state std_srvs/srv/Trigger"
+docker run -it --rm -d\
+    --env=ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
+    --net=host \
+    --volume=$(dirname $(dirname $(realpath $0)))/inputs:/inputs:rw \
+    --name=stella_vslam-rosbag \
+    stella_vslam-rosbag:latest \
+    /inputs/start_stella_vslam-rosbag.sh
+# docker exec -it -d component_stella_docker /bin/bash -c ". /ros2_ws/install/setup.bash && ros2 service call /component/change_state std_srvs/srv/Trigger"
 
 read -p "Press [Enter] key to finish all processes and kill the docker"
 
-docker kill component_docker
+docker kill stella_vslam-ros-socket
+docker kill stella_vslam-rosbag
